23fc222af2a4e01c8b8a7fe2f9a3a5f190a65380
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-03-08 12:51:03 +0700

    input

--- 125_BubbleSort/125_BubbleSort.cpp
+++ 125_BubbleSort/125_BubbleSort.cpp
@@ -1,9 +1,29 @@
 
 #include <iostream>
+using namespace std;
 
+int a[20];
+int n;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void input() {
+	while (true) {
+		cout << " Masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20.\n";
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
 }
+
 
